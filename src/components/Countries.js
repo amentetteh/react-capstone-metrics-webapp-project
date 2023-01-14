efa623a2fa7bf28275f2360fc8e4c41c22bc451d
Amen TETTEH <tkamen@outlook.com>
2023-01-14 05:15:28 +0000

    Displat data from APIe

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,8 +1,70 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { NavLink, useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { nanoid } from 'nanoid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { fetchCountries } from '../redux/countries/countriesSlice';
 
-const Countries = () => (
-  <>
-    Country
-  </>
-);
+const Countries = () => {
+  const dispatch = useDispatch();
+  const countries = useSelector((state) => state.countries.countries);
+  const { id } = useParams();
+  const reg = id ? id.toLowerCase() : 'ame';
+
+  useEffect(() => {
+    if (countries.length === 0) {
+      dispatch(fetchCountries(reg));
+    }
+  }, [dispatch, countries.length, countries.refresh, reg]);
+  return (
+    <div className="country-container">
+      {countries.loading && <div className="loading" />}
+      {!countries.loading && countries.error && (
+        <div className="error">
+          Error:
+          {countries.error}
+        </div>
+      )}
+      {!countries.loading && countries.length && (
+        <>
+          <header>
+            {/* <img src={banner} alt="banner" /> */}
+            <div className="search-field">
+              <FontAwesomeIcon className="App-backtohome" icon={faSearch} />
+              <input
+                type="search"
+                placeholder="Search Region"
+              />
+              <select name="selected">
+                <option> * </option>
+                <option value="Asia"> Asia</option>
+                <option value="Oceania">Oceania </option>
+                <option value="Europe"> Europe </option>
+                <option value="Americas"> Americas</option>
+                <option value="Africa"> Africa</option>
+              </select>
+            </div>
+          </header>
+          <ul className="country-list">
+            {countries.map((country) => (
+              <li className="country-item" key={nanoid()}>
+                <NavLink
+                  to={`/pollution/${country.latlng};${
+                    country.name.common
+                  };${country.altSpellings[0].toLowerCase()};${country.population}`}
+                >
+                  {country.name.common}
+                  {country.capital}
+                  {country.languages.eng}
+                  <img src={country.flags.png} alt="Country Flag" />
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
 export default Countries;
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,9 +1,112 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAirPollutionInfos } from '../redux/airpollution/airPollutionInfosSlice';
+import airRiskRate from '../Util/Util';
 
 function Details() {
+  const dispatch = useDispatch();
+  const params = useParams();
+  const { infos, loading } = useSelector((state) => state.infos);
+  const paramsArray = params.id.split(';');
+  const cord = paramsArray[0].split(',');
+  const countryName = paramsArray[1];
+  const countryCode = paramsArray[2];
+  const countryFlag = `https://flagcdn.com/w320/${countryCode}.png`;
+  const countryPopulation = paramsArray[3];
+  const { aqi, data } = infos;
+  const airQuality = airRiskRate(aqi);
+  useEffect(() => {
+    if (infos.length === 0) {
+      dispatch(getAirPollutionInfos(cord));
+    }
+  }, [dispatch, infos]);// eslint-disable-line react-hooks/exhaustive-deps
   return (
     <>
-      Details
+      {loading && <div className="loading" />}
+      {!loading && data && (
+      <>
+        <div className="pd-details">
+          <div className="hero">
+            <img src={countryFlag} alt="Flag" />
+            <h2>
+              Country :
+              {countryName}
+            </h2>
+            <h2>
+              Population :
+              {countryPopulation}
+            </h2>
+          </div>
+          <div className="details">
+            <h2>
+              Air Pollution Details:
+            </h2>
+            <ul>
+              <li>
+                <span> Carbon monoxide (CO) </span>
+                <span>{ data.co }</span>
+              </li>
+              <li>
+                <span>
+                  Ammonia ( NH
+                  <sub>3</sub>
+                  )
+                </span>
+                <span>{ data.nh3 }</span>
+              </li>
+              <li>
+                <span>
+                  Nitrogen monoxide (NO)
+                </span>
+                <span>{ data.no }</span>
+              </li>
+              <li>
+                <span>
+                  Nitrogen dioxide (NO
+                  <sub>2</sub>
+                  )
+                </span>
+                <span>{ data.no2 }</span>
+              </li>
+              <li>
+                <span>
+                  Ozone (O
+                  <sub>3</sub>
+                  )
+                </span>
+                <span>{ data.pm2_5 }</span>
+              </li>
+              <li>
+                <span>
+                  {' '}
+                  Fine particles matter(PM2_5)
+                  {' '}
+                </span>
+                <span>{ data.pm10 }</span>
+              </li>
+              <li>
+                <span>
+                  {' '}
+                  Coarse particulate matter (PM_10)
+                  {' '}
+                </span>
+                <span>{ data.pm10 }</span>
+              </li>
+              <li>
+                <span>
+                  {' '}
+                  Sulphur dioxide (SO2)
+                  {' '}
+                </span>
+                <span>{ data.so2 }</span>
+              </li>
+            </ul>
+            <span>{ airQuality }</span>
+          </div>
+        </div>
+      </>
+      )}
     </>
   );
 }
